4bd82817d91fec24cdf6b26d4fd2b4c5e8e10407
jyoti <jyotidhage10@gmail.com>
2022-08-07 09:56:02 +0530

    [Aaradhya]Add:Welcome To Employee Wage Computation

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace EmployeeWages
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // EmployeePresentAbsent.EmployeePresentOrAbsent();
+            //DailyEmpwage.EmployeeDailyWage();
+            // PartTimeEmployeeWage.PartTimeWage();
+           // SwitchCaseStatement.CaseStatement();
+
+            CalculateWages.calculateHrswages();
+
+            //MonthlyEmpWage.MontlyWageComputation()//
+        }
+    }
+    }
+
